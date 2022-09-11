bfe63aaa4525e7c29160a1703aaee80c834c8c2d
agon <agonhaxhani83@gmail.com>
2022-09-11 20:50:34 +0200

    save job on db

--- src/main/java/com/akropoliprishtine/crawler/Scheduler.java
+++ src/main/java/com/akropoliprishtine/crawler/Scheduler.java
@@ -21,7 +21,7 @@ public class Scheduler {
     @Autowired
     DailyJobService dailyJobService;
 
-    @Scheduled(cron = "0 50 21 * * *")
+    @Scheduled(cron = "0 00 22 * * *")
     public void runScheduler() {
         DayOfWeek dayOfWeek = LocalDate.now().getDayOfWeek();
         List<DailyJob> jobs = new ArrayList<>();
